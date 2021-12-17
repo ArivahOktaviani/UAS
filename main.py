2ebc6be14ca086fb81bc65b5db0c7207e77521e1
ArivahOktaviani <96269933+ArivahOktaviani@users.noreply.github.com>
2021-12-17 06:04:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -63,8 +63,7 @@ ax.set_xlabel("Tahun", color="green", fontsize = 20)
 ax.set_ylabel("Jumlah Produksi", color="yellow", fontsize = 20)
 ax.legend(fontsize = 20)
 plt.scatter("Tahun", "Jumlah Produksi", color="yellowgreen", marker='x', label='item 1')
-plt.show()
-
+left_col.pyplot(fig)
 
 
 
