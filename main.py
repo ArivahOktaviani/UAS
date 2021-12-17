94e88c204c402eb677376473d25dbc6ea534ba2b
ArivahOktaviani <96269933+ArivahOktaviani@users.noreply.github.com>
2021-12-17 06:09:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -54,7 +54,6 @@ df2['produksi'] = pd.to_numeric(df2['produksi'], errors='coerce')
 left_col.write(df2)
 #Grafik Negara dengan Produksi 
 fig, ax = plt.subplots()
-ax.plot(df2['tahun'], df2['produksi'], label = df2['tahun'])
 ax.set_title("Jumlah Produksi Per Tahun di Negara Pilihan")
 ax.set_xlabel("Tahun", color="green", fontsize = 20)
 ax.set_ylabel("Jumlah Produksi", color="yellow", fontsize = 20)
